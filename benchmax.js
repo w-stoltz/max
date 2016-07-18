a982e0a622c1978ef2d08f135b6696c1aaca5484
grstoltz <grant@grantstoltz.com>
2016-07-17 20:46:26 -0700

    Updated JS
    adds new variables, creates a working copy

--- benchmax.js
+++ benchmax.js
@@ -1,23 +1,32 @@
-var bench = document.getElementById('bench').value
-if (bench > 500) {
-  console.log("Shut up liar");
-  var bench = prompt("What is your bench");
+window.onload = function (){
+
+var form = document.getElementById("benchMaxForm");
+    weight = document.getElementById("benchWeight");
+    reps = document.getElementById("benchReps");
+
+form.onsubmit = function() {
+   var benchWeight = weight.value;
+   var benchReps = reps.value;
+   console.log(benchWeight);
+   console.log(benchReps)
+
+if (benchWeight > 500) {
+  alert("Shut up liar");
 }
-else if(isNaN(bench)){
-  console.log("Put in a real number")
-  var bench = prompt("What is your bench");
+else if(isNaN(benchWeight)){
+  alert("Put in a real number")
+}
+if (benchReps > 20) {
+  alert("Do less cardio");
+}
+else if(isNaN(benchReps)){
+  alert("u wot")
+}
+var repArray = [0, 1, 1.07, 1.12, 1.15, 1.18, 1.21, 1.24, 1.27, 1.30, 1.33];
+var bRep = repArray[benchReps];
+var benchMax = benchWeight * bRep;
+alert("You can max bench " + benchMax);
+
+};
 }
 
-var benchreps = prompt("How many reps");
-if (benchreps > 20) {
-  console.log("Do less cardio");
-  var benchreps = prompt("How many reps");
-}
-else if(isNaN(benchreps)){
-  console.log("u wot")
-  var benchreps = prompt("how many reps");
-}
-var reps = [0, 1, 1.07, 1.12, 1.15, 1.18, 1.21, 1.24, 1.27, 1.30, 1.33];
-var bRep = reps[benchreps];
-benchMax = bench * bRep;
-console.log(benchMax);
